bde29a4687764a90bbbf6f5cdf44b1e86c4b0ff8
zeroway <M_O_Bz@163.com>
2021-02-16 22:12:52 +0000

    Add color scheme macro

--- config.h
+++ config.h
@@ -93,6 +93,24 @@ unsigned int tabspaces = 8;
 /* bg opacity */
 float alpha = 0.92;
 
+/* Solarized dark color scheme */
+#define S_base03        "#002b36"
+#define S_base02        "#073642"
+#define S_base01        "#586e75"
+#define S_base00        "#657b83"
+#define S_base0         "#839496"
+#define S_base1         "#93a1a1"
+#define S_base2         "#eee8d5"
+#define S_base3         "#fdf6e3"
+#define S_yellow        "#b58900"
+#define S_orange        "#cb4b16"
+#define S_red           "#dc322f"
+#define S_magenta       "#d33682"
+#define S_violet        "#6c71c4"
+#define S_blue          "#268bd2"
+#define S_cyan          "#2aa198"
+#define S_green         "#859900"
+
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
 	"#282828", /* hard contrast: #1d2021 / soft contrast: #32302f */
@@ -115,8 +133,8 @@ static const char *colorname[] = {
 	/* more colors can be added after 255 to use with DefaultXX */
 	"#add8e6", /* 256 -> cursor */
 	"#555555", /* 257 -> rev cursor*/
-	"#073642", /* 258 -> bg */
-	"#268bd2", /* 259 -> fg */
+	S_base02, /* 258 -> bg */
+	S_base00, /* 259 -> fg */
 };
 
 
